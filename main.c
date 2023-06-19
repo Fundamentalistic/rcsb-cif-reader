466b1be6e45098ac282d0ee2754314868702c5f5
Bespalov Andrei <a.bespalov@alumni.nsu.ru>
2023-06-19 21:38:42 +0700

    fix(bespalob/): parameters mark

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 int main(int argc, char * argv[])
 {
+    int is_aa_sequence_builder_required = 0;
 
     printf("CIF stat reading...");
     
@@ -12,7 +13,6 @@ int main(int argc, char * argv[])
     for(int i = 0; i < params->chains_count; i++){
         printf("chain: %5i\tatoms_size: %5u\tres_size: %5u\n", i + 1, params->chainsParameters[i].atoms_len, params->chainsParameters[i].residues_len);
     }
-
     printf("chains_count: %u\n", params->chains_count);
     printf("residues_count: %u\n", params->residues_count);
     printf("atoms_count: %u\n", params->atoms_count);
@@ -21,12 +21,14 @@ int main(int argc, char * argv[])
 
     printf("cif object is generated\n");
 
-    for(int i = 0; i < params->chains_count; i++){
-        printf("chain_id: %i\n", i);
-        for(int j = 0; j < cif_file->chains[i].residue_size; j++){
-            printf("%s->", cif_file->chains[i].residues[j].res_name);
+    if(is_aa_sequence_builder_required){
+        for(int i = 0; i < params->chains_count; i++){
+            printf("chain_id: %i\n", i);
+            for(int j = 0; j < cif_file->chains[i].residue_size; j++){
+                printf("%s->", cif_file->chains[i].residues[j].res_name);
+            }
+            printf("\b\b  \n\n");
         }
-        printf("\b\b  \n\n");
     }
 
     free(cif_file);
